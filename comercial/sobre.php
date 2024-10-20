2188de94c0465335399432bf68a466ce73a86681
Vitor Brancatti <168239976+VitorBrancatti@users.noreply.github.com>
2024-10-20 02:34:03 -0300

    Add files via upload

---
+++ comercial/sobre.php
@@ -0,0 +1,20 @@
+<?php
+	include_once("cabec.php")
+?>
+
+	<p>&nbsp;</p>
+
+	<h2 align="center">Sobre o Sistema</h2>
+
+	<p>&nbsp;</p>
+	<p>&nbsp;</p>
+
+	<h3 align="center">Sistema desenvolvido por Vitor e Enzo </h3>
+
+	<p>&nbsp;</p>
+
+	<h3 align="center">Contato: adm@gmail.com</h3>
+
+<?php
+	include_once("rodape.php");
+?>
